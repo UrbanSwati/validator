08727e98c9ff45dbb02324fd4a2a2ca8c2814dcd
William <williesdk@gmail.com>
2018-05-05 01:40:31 +0200

    Added Java Doc for validPassword method

--- src/main/java/Validations.java
+++ src/main/java/Validations.java
@@ -61,16 +61,30 @@ public class Validations
     }
 
 
+    /**
+     * Method to validate a strong password
+     *
+     * a digit must occur at least once
+     * a lower case letter must occur at least once
+     * an upper case letter must occur at least once
+     * a special character must occur at least once
+     * no whitespace allowed in the entire string
+     * at least eight characters length
+     *
+     * @param password users password
+     * @return boolean
+     */
     public boolean validPassword(String password)
     {
-        //        ^                 # start-of-string
-        //        (?=.*[0-9])       # a digit must occur at least once
-        //        (?=.*[a-z])       # a lower case letter must occur at least once
-        //        (?=.*[A-Z])       # an upper case letter must occur at least once
-        //        (?=.*[@#$%^&+=])  # a special character must occur at least once
-        //        (?=\S+$)          # no whitespace allowed in the entire string
-        //        .{8,}             # anything, at least eight places though
-        //          $               # end-of-string
+        /*       ^                # start-of-string
+        *        (?=.*[0-9])       # a digit must occur at least once
+        *        (?=.*[a-z])       # a lower case letter must occur at least once
+        *        (?=.*[A-Z])       # an upper case letter must occur at least once
+        *        (?=.*[@#$%^&+=])  # a special character must occur at least once
+        *        (?=\S+$)          # no whitespace allowed in the entire string
+        *        .{8,}             # anything, at least eight places though
+        *         $                # end-of-string
+        */
 
         String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!^&+=])(?=\\S+$).{8,}$";
 
